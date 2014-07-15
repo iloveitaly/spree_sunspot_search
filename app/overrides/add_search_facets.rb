88057537cb329c8fee8fa45a30b07ceef7213351
Michael Bianco <iloveitaly@gmail.com>
2014-07-15 06:14:05 -0400

    Adding :original to deface definitions

--- app/overrides/add_search_facets.rb
+++ app/overrides/add_search_facets.rb
@@ -2,4 +2,5 @@ Deface::Override.new(:virtual_path => "spree/shared/_taxonomies",
                       :name => "show_search_partials_facets",
                       :insert_top => "nav#taxonomies",
                       :partial => "spree/products/facets",
+                      :original => '92a1326a3d32fa80f3af3a749b99377694ae39bc',
                       :disabled => false)
--- app/overrides/add_search_sort.rb
+++ app/overrides/add_search_sort.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :name => "add_sort_bar",
                      :insert_before => "#products",
+                     :original => '40b506d0e09adb9ef2a9e53f6de9bc2575495a08',
                      :partial => 'spree/products/sort_bar')
