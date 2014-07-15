a5e2cfa8ce3abdb267e47507cf5a629b2968fa01
Michael Bianco <iloveitaly@gmail.com>
2014-07-15 06:14:20 -0400

    Fixing pagination override

--- app/overrides/add_search_pagination.rb
+++ app/overrides/add_search_pagination.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products",
                       :name => "add_sunspot_search_pagination",
-                      :replace => "code[erb-silent]:contains('if paginated_products.respond_to')",
-                      :closing_selector => "code[erb-silent]:contains('end')",
+                      :original => 'de1f2c6acd14c3b392c06a5760f54ab4b525ff6c',
+                      :replace => "erb[silent]:contains('if paginated_products.respond_to')",
+                      :closing_selector => "erb[silent]:contains('end')",
                       :text => "<%= paginate @searcher.sunspot.hits %>")
